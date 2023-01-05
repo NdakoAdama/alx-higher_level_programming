59ba3f651793822b9a620d8606512ca22cdd9fbb
NdakoAdama <adamamuhammad01@gmail.com>
2023-01-05 06:16:30 -0800

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-""" text_indentation returns "text" in the specified format:
-2 newlines after each ['.', '?', ':']
+"""
+This module defines `text_indentation`
+The function prints a text with 2 new lines after each of
+these characters: ., ? and :
 """
 
 
 def text_indentation(text):
-    """ prints "text" with 2 newlines after each of these char: ['.', '?', ':']
-    checks if "text" is a str
-    first loop removes spaces after each required chars
-    second loop adds 2 newlines after each required chars
+    """adds paragraph after `.`, `:` and `?`
+    Args:
+        text (str)
     """
-    if type(text) != str:
-        raise TypeError("text must be a string")
-    toCatAfter = ['.', '?', ':']
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
 
-    # Removes the space after special chars
-    idx = 0
-    for items in text:
-        if items in toCatAfter:
-            if text[idx + 1] == " ":
-                text = text[:idx + 1] + text[idx + 2:]
-        else:
-            idx += 1
-
-    # Cats '\n\n' after the special char with removed space
-    idx = 0
-    for items in text:
-        if items in toCatAfter:
-            text = text[:idx + 1] + '\n\n' + text[idx + 1:]
-            idx += 3
-        else:
-            idx += 1
-
-    print(text, end='')
+    for l in '.:?':
+        text = text.replace(l, '{}\n'.format(l))
+    lines = text.splitlines()
+    for index, line in enumerate(lines):
+        print(line.strip(), end='' if index == len(lines) - 1 else '\n\n')
