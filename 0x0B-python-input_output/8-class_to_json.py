6956c5e543c8682e1f9a2804c51460da8dc61881
NdakoAdama <adamamuhammad01@gmail.com>
2023-01-10 22:43:19 -0800

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
 """
-Contains class Student
-that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that retrieves its dictionary representation
+Contains function that
+returns dictionary description with simple data structure
+(list, dictionary, dictionary, string)
+for JSON serialization of an object
 """
 
 
-class Student():
+def class_to_json(obj):
+    """Returns dictionary description with simple data structure
+       (list, dictionary, dictionary, string)
+       for JSON serialization of an object
+    Args:
+        obj: python object
     """
-    Public Attributes:
-        first_name
-        last_name
-        age
-    Public Methods:
-        to_json: retrieves its dictionary representation
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-        Initializes student with full name and age
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
-        """
-        return self.__dict__
+    return obj.__dict__
